ce639be5ae1d59754a44387a7d81d71a04a49513
ponchorolls <willvatcher@gmail.com>
2025-12-31 08:51:17 -0800

    Nix flake for dev env

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Social Distance (sd) Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Backend Runtime
+            nodejs_20
+            
+            # Databases (The Engines)
+            redis
+            postgresql_15
+            
+            # Tools
+            git
+            nodePackages.typescript-language-server
+          ];
+
+          shellHook = ''
+            echo "--- Social Distance (sd) Dev Environment ---"
+            echo "Redis and Postgres are available in your path."
+            echo "Run 'redis-server' to start the leaderboard engine."
+          '';
+        };
+      });
+}
